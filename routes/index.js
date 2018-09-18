99ac5ac87371956ac4a748cb44da4515cc7df2e2
udaypb <uday.pb26@gmail.com>
2018-09-18 18:31:09 +0530

    added login

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,36 @@ router.post('/signup',function(req,res,next){
   var username=req.body.username;
   var email=req.body.email;
   console.log('signup called');
-  
- 
 
 });
+/**get Selection page */
+router.get('/selection',function(req,res,next){
+  //get data for that user here:
+  var uid=req.query.user;
+  //verify idToken
+  firebase.auth().verifyIdToken(uid)
+  .then(function(decodedToken) {
+    let uid = decodedToken.uid;
+    //get the user
+    firebase.auth().getUser(uid).then(function(userRecord) {
+    // See the UserRecord reference doc for the contents of userRecord.
+    console.log("Successfully fetched user data:", userRecord.toJSON());
+    //now render selection page:
+    res.render('selection',{'userData':userRecord.toJSON()});
+  })
+  .catch(function(error) {
+    console.log("Error fetching user data:", error);
+  });
+    // ...
+  }).catch(function(error) {
+    // Handle error
+    console.log('user not logged in')
+    //redirect to login:
+    res.redirect('/login');
+  });
+
+
+})
 
 
 module.exports = router;
